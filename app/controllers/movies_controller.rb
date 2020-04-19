ad0481497fc72bb33e44e8f9f1d4a2724e563e47
gavinsharkey <gavinsharkey2016@gmail.com>
2020-04-19 11:01:24 -0400

    Configure show and index route of MoviesController add basic html to movies show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,13 @@
 class MoviesController < ApplicationController
+  
+  get '/movies' do
+    redirect_if_not_logged_in
+    @movies = current_user.movies
+    erb :'movies/index'
+  end
+  
   get '/movies/new' do
-    
+    redirect_if_not_logged_in
   end
 
   post '/movies' do
@@ -8,11 +15,17 @@ class MoviesController < ApplicationController
   end
 
   get '/movies/:id' do
-    
+    redirect_if_not_logged_in
+    @movie = current_user.movies.find(params[:id])
+    if @movie
+      erb :"movies/show"
+    else
+      redirect '/jars'
+    end
   end
 
   get '/movies/:id/edit' do
-    
+    redirect_if_not_logged_in
   end
 
   patch '/movies/:id' do
